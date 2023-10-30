827dded019a626965344cb8b664315ba795c6550
YiY <zldeyexiu@gmail.com>
2023-10-30 17:39:58 +0800

    2418. 按身高排序

--- 00_Sort/2418. 按身高排序/sortPeople.js
+++ 00_Sort/2418. 按身高排序/sortPeople.js
@@ -1,8 +1,16 @@
 var sortPeople = function (names, heights) {
   // let arr = JSON.parse(JSON.stringify(heights)).sort((a, b) => b - a)
-  let arr = [...heights].sort((a, b) => b - a)
+  // let arr = [...heights].sort((a, b) => b - a)
 
-  return arr.map(item => names[heights.indexOf(item)])
+  // 2.2
+  return names.map((e, i) => [heights[i], e])
+    .sort((a, b) => b[0] - a[0])
+    .map(i => i[1])
+
+  // 2.1
+  // return arr.map(item => names[heights.indexOf(item)])
+
+  // 1.0
   // return arr.reduce((res, item) => {
   //   res.push(names[heights.findIndex(value => value == item)])
   //   return res
